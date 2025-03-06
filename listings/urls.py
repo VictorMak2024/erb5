03cb7dc3f6b6c4f0ce17601746a397f3e101845b
Victor Mak <victormakwaitat@gmail.com>
2025-03-06 12:47:29 +0800

    Step 6 : Pagination

--- listings/urls.py
+++ listings/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.listings, name='listings'),
-    path('<int:listing_id>', views.listings, name='listing'),
+    path('<int:listing_id>', views.listing, name='listing'),
     path('search', views.search, name='search'),
 ]
--- listings/views.py
+++ listings/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
 
+# import class Listing from listings/models.py 
+from . models import Listing
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 # Create your views here.
 
 def listings(request):
-    return render(request, 'listings/listings.html', {'listings' : 'something'})
+    # .objects.all() -> take all data from databases
+    #listings = Listing.objects.all()
+    #In Django, .order_by(param), .filter(param) <- Query Set (build sql command)
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
+    paginator = Paginator(listings, 3)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
+    context = {'listings': paged_listings}
+    return render(request, 'listings/listings.html', context)
 
-def listing(request):
+#listings/urls.py <- urlpatterns
+def listing(request, listing_id):
     return render(request, 'listings/listing.html')
 
 def search(request):
