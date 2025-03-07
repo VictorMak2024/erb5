03a8b49721067dd5862a47c01f08dcb20fd33aba
Victor Mak <victormakwaitat@gmail.com>
2025-03-07 17:40:10 +0800

    Step 7 : update listings templates

--- listings/views.py
+++ listings/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # import class Listing from listings/models.py 
 from . models import Listing
@@ -8,7 +8,18 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def listings(request):
     # .objects.all() -> take all data from databases
-    #listings = Listing.objects.all()
+    """
+    # listings = Listing.objects.filter() # In (), using QuerySet API
+    # listings = Listing.objects.filter().exist()
+    # print(listings)
+    # listings = Listing.objects.get(id=1) # final record
+    #print(listings)
+    listings = Listing.objects.all()
+    # print data from database
+    print(list(listings))
+    # print data from database
+    for listing in listings:
+        print(listing)"""
     #In Django, .order_by(param), .filter(param) <- Query Set (build sql command)
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)
     paginator = Paginator(listings, 3)
@@ -19,7 +30,9 @@ def listings(request):
 
 #listings/urls.py <- urlpatterns
 def listing(request, listing_id):
-    return render(request, 'listings/listing.html')
+    listing = get_object_or_404(Listing, pk=listing_id)
+    context = {'listing': listing}
+    return render(request, 'listings/listing.html', context)
 
 def search(request):
     return render(request, 'listings/search.html')
