f4e1068256cbe66f932a351c6954b705f744e92e
Victor Mak <victormakwaitat@gmail.com>
2025-03-19 17:51:09 +0800

    Step 14: fix sendemail

--- bcre/settings.py
+++ bcre/settings.py
@@ -161,10 +161,11 @@ MESSAGE_TAGS = {
 }
 
 # Email Config
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_PORT = 587 # HKCT blocks SMTP
 # your email address
-EMAIL_HOST_USER = ''
-# youremail password
+EMAIL_HOST_USER = 'victormakwaitat@gmail.com'
+# youremail password <- google 16 digit number
 EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
